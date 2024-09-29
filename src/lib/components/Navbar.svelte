19ff6371e3d1ca74c6ea94fbcfce0e1ae4f96005
LoneWolf <imalkesara2001@gmail.com>
2024-09-29 13:07:51 +0530

    svelte 5

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+    type Children ={
+        children :Snippet
+    }
+
+    let {children}: Children = $props()
+</script>
+
+<nav class="flex items-center justify-center bg-primary px-4 text-primary-foreground">
+    {@render children()}
+</nav>
