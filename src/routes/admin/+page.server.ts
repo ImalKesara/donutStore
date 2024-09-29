d8e7c6bba77aec397cc1f848e8d983f91717bd74
LoneWolf <imalkesara2001@gmail.com>
2024-09-30 00:14:53 +0530

    getSaledata func

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,5 +1,17 @@
-import type { PageServerLoad } from './$types';
+import { db } from '../../hooks.server';
 
-export const load = (async () => {
+async function getSalesData() {
+	const data = await db.order.aggregate({
+		// means priceIncents tiyena ewaye withrk ekathuwa
+		_sum: { priceInCents: true },
+		_count: true
+	});
+	return {
+		amount: (data._sum.priceInCents || 0) / 100,
+		numberOfSales: data._count
+	};
+}
+
+export const load = async () => {
 	return {};
-}) satisfies PageServerLoad;
+};
