8dff6efcc291ac89f722665d114b6b4ed8306337
LoneWolf <imalkesara2001@gmail.com>
2024-09-30 17:35:25 +0530

    load all products

--- src/routes/admin/products/+page.server.ts
+++ src/routes/admin/products/+page.server.ts
@@ -1,5 +1,21 @@
-import type { PageServerLoad } from './$types';
+import { db } from '../../../hooks.server';
 
-export const load = (async () => {
-	return {};
-}) satisfies PageServerLoad;
+export const load = async () => {
+	return {
+		products: await db.product.findMany({
+			select: {
+				id: true,
+				name: true,
+				priceInCents: true,
+				isAvailableForPurchase: true,
+				imagePath: true,
+				_count: {
+					select: {
+						order: true
+					}
+				}
+			},
+			orderBy: { name: 'asc' }
+		})
+	};
+};
