d1b77bccf9d381552c1d2ce96d26de95cacf9c36
LoneWolf <imalkesara2001@gmail.com>
2024-09-30 16:56:57 +0530

    page header component reuse header

---
+++ src/lib/components/PageHeader.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type PageHeaderType = {
+		children: Snippet;
+	};
+	let { children }: PageHeaderType = $props();
+</script>
+
+<div class="mb-4 text-2xl font-semibold">
+	{@render children()}
+</div>
