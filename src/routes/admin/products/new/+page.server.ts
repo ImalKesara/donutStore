3b30c274a7f8f4abd97c499e1947f6979469af0d
LoneWolf <imalkesara2001@gmail.com>
2024-10-04 01:13:56 +0530

    create product fuc=nc

--- src/routes/admin/products/new/+page.server.ts
+++ src/routes/admin/products/new/+page.server.ts
@@ -1,9 +1,50 @@
 import { addFormSchema } from '$lib/formSchema';
-import { superValidate } from 'sveltekit-superforms';
+import { superValidate, fail } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
+import fs from 'fs/promises';
+import { db } from '../../../../hooks.server.js';
+import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {
 	return {
 		form: await superValidate(zod(addFormSchema))
 	};
+};
+
+export const actions = {
+	default: async ({ request }) => {
+		const form = await superValidate(request, zod(addFormSchema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		// create products-file folder
+		await fs.mkdir('productFile', { recursive: true });
+		// file path
+		const filePath = `productFile/${crypto.randomUUID()}-${form.data.name}`;
+		await fs.writeFile(filePath, Buffer.from(await form.data.file.arrayBuffer()));
+
+		// create products-image folder
+		await fs.mkdir('static/productsIMG', { recursive: true });
+		// image path
+		const imagePath = `static/productsIMG/${crypto.randomUUID()}-${form.data.image.name}`;
+		await fs.writeFile(imagePath, Buffer.from(await form.data.image.arrayBuffer()));
+
+		try {
+			await db.product.create({
+				data: { 
+					name: form.data.name,
+					description: form.data.description,
+					priceInCents: form.data.priceInCents,
+					filePath,
+					imagePath,
+					isAvailableForPurchase: false
+				}
+			});
+		} catch (e) {
+			console.error(e);
+		}
+
+		redirect(303, '/admin/products');
+	}
 };
