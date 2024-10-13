10056cd62db29fe2cf793761e5b98d583478410f
LoneWolf <imalkesara2001@gmail.com>
2024-10-13 16:40:30 +0530

    added stripe

--- src/routes/(customerFacing)/products/[id]/purchase/+page.server.ts
+++ src/routes/(customerFacing)/products/[id]/purchase/+page.server.ts
@@ -1,16 +1,34 @@
 import { error } from '@sveltejs/kit';
 import { db } from '../../../../../hooks.server';
+import Stripe from 'stripe';
+import { STRIPE_SECRETE_KEY } from '$env/static/private';
+
+
+const stripe = new Stripe(STRIPE_SECRETE_KEY)
 
 export const load = async ({ params: { id } }) => {
 	const product = await db.product.findUnique({
 		where: { id }
 	});
 
-	if (!product) 
-		error(404,'not found')
-	
+	if (!product) error(404, 'not found');
+	const payementIntent = await stripe.paymentIntents.create({
+		amount:product.priceInCents,
+		currency:'USD',
+		metadata:{
+			productId : product.id
+		},
+		automatic_payment_methods:{
+			enabled:true
+		}
+	})
+
+	if(payementIntent.client_secret === null){
+		throw Error('Failed to create payment intent')
+	}
+
 	return {
-		product
+		product,
+		clientSecrete : payementIntent.client_secret
 	};
 };
- 
--- src/routes/(customerFacing)/products/[id]/purchase/+page.svelte
+++ src/routes/(customerFacing)/products/[id]/purchase/+page.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
+	import { Loader } from 'lucide-svelte';
 	import { page } from '$app/stores';
+	import { PUBLIC_STRIPE_PUBLIC_KEY } from '$env/static/public';
 	import { Button } from '$lib/components/ui/button';
 	import { formatCurrency } from '$lib/utils.js';
+	import { loadStripe } from '@stripe/stripe-js';
+	import { onMount } from 'svelte';
+	import { Elements, PaymentElement, Address, LinkAuthenticationElement } from 'svelte-stripe';
+	import * as Card from '$lib/components/ui/card';
+
+	let stripe: any = $state();
+	let elements: any = $state(undefined);
+	let isLoading = $state(false);
+	let errorMsg: string | undefined = $state(undefined);
+	let error: string | null = $state(null);
+
+	onMount(async () => {
+		stripe = await loadStripe(PUBLIC_STRIPE_PUBLIC_KEY);
+	});
+
 	let { data } = $props();
+	let productId = $derived(data.product.id);
 </script>
 
 <div class="mx-auto grid h-[750px] w-full max-w-5xl items-center space-y-8">
@@ -13,10 +31,41 @@
 			class="h-[450px] w-[600px] rounded-lg object-cover"
 		/>
 		<div class="grid grid-rows-8 p-5">
-			<h1 class="row-span-2 text-3xl font-black">{data.product.name}</h1>
-			<p class="row-span-">{data.product.description}</p>
+			<h1 class="row-span-1 text-3xl font-black">{data.product.name}</h1>
+			<p class="row-span-1">{data.product.description}</p>
 			<p class="text-sm text-muted-foreground">{formatCurrency(data.product.priceInCents / 100)}</p>
-			<Button>Buy</Button>
+			
+			<!-- <Button>Buy</Button> -->
 		</div>
 	</div>
+	<!-- stripe payment form -->
+	<Elements clientSecret={data.clientSecrete} {stripe} bind:elements>
+		<form>
+			<Card.Root>
+				<Card.Header>
+					<Card.Title>Checkout</Card.Title>
+					<Card.Description>Card Description</Card.Description>
+				</Card.Header>
+				<Card.Content>
+					<LinkAuthenticationElement />
+					<PaymentElement />
+					<Address mode="billing" />
+				</Card.Content>
+				<Card.Footer>
+					<Button
+						type="submit"
+						color="primary"
+						size="lg"
+						disabled={stripe === null || elements === undefined}
+					>
+						{#if isLoading}
+							<Loader class="size-4 animate-spin" />
+						{:else}
+							Purchase - {formatCurrency(data.product.priceInCents/100)}
+						{/if}
+					</Button>
+				</Card.Footer>
+			</Card.Root>
+		</form>
+	</Elements>
 </div>
